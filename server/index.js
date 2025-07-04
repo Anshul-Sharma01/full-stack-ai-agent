43b2645ffaa91f92ed0d332d33da6f252b217bda
Anshul Sharma <sharma.ansh2926@gmail.com>
2025-07-04 11:28:52 +0530

    --Backend Completed
    integrate Inngest functions and enhance ticket routes with authentication; add new dev script for Inngest CLI

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
+import { serve } from "inngest/express";
+import { inngest } from "./inngest/client.js";
+import { onUserSignup } from "./inngest/functions/onSignup.js";
+import { onTicketCreated } from "./inngest/functions/onTicketCreate.js";
 
 dotenv.config();
 
@@ -13,8 +17,15 @@ app.use(morgan("dev"));
 
 
 import useRoutes from "./routes/user.routes.js";
+import ticketRoutes from "./routes/ticket.routes.js";
 
 app.use("/api/auth", useRoutes);
+app.use("/api/ticket", ticketRoutes);
+
+app.use("/api/inngest", serve({
+    client : inngest,
+    functions : [onUserSignup, onTicketCreated]
+}))
 
 mongoose
     .connect(process.env.MONGO_URI)
--- server/routes/ticket.routes.js
+++ server/routes/ticket.routes.js
@@ -1,8 +1,15 @@
 import express, { Router } from "express";
-
+import { createTicket, getTicket, getTickets } from "../controllers/ticket.controllers";
+import { authenticateUser } from "../middlewares/auth.middleware.js";
+import { } from "../controllers/ticket.controllers.js";
 
 const router = Router();
 
+router.use(authenticateUser);
+
+router.get("/", getTickets);
+router.get("/:id", getTicket);
+router.post("/", createTicket);
 
 export default router;
 
